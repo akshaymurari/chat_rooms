80c68440c4157ce1de2f9ac433d586d631d88964
akshaymurari <akshaymurari184@gmail.com>
2021-11-07 22:37:05 +0530

    added online users status feature

--- backend/app.js
+++ backend/app.js
@@ -50,12 +50,61 @@ const ws_router = require("./wsrouter");
 
 const {parse} = require("url");
 
+var online_users = [];
+
+var bool_users = new Map();
+
 wss.on("connection", (ws,request) => {
     //store a channel name
+    console.log(bool_users);
     var roomId = request.url.substring(1,request.url.length);
     ws.on("message", async (message) => {
         let msg = JSON.parse(message);
-        ws_router(ws,msg,roomId);
+        if(msg.type==="ONLINE"){
+            if(!bool_users.get(msg.username)){
+                ws.user = msg.username;
+                online_users.push(msg.username);
+                bool_users.set(msg.username,true);
+                // console.log("online users",online_users.length);
+                wss.clients.forEach(client => {
+                    if(client.readyState === WebSocket.OPEN){
+                        client.send(JSON.stringify({
+                            type:"ONLINE",
+                            online_users
+                        }));
+                    }
+                });
+            }else{
+                ws.send(JSON.stringify({
+                    type:"ONLINE",
+                    online_users
+                }));
+            }
+        }else{
+            ws_router(ws,msg,roomId);
+        }
+    });
+
+    ws.on("close", () => {
+        console.log("closed");
+        bool_users.set(ws.user,false);
+        online_users = online_users.filter(user => user !== ws.user);
+        // console.log("online users",online_users.length);
+        // i=0
+        wss.clients.forEach(client => {
+            // i+=1;
+            if(client.readyState === WebSocket.OPEN){
+                client.send(JSON.stringify({
+                    type:"ONLINE",
+                    online_users
+                }));
+            }
+            // console.log(i);
+        });
+    });
+
+    ws.on("error", (err) => {
+        console.log("error",err);
     });
 
     // ws.on("close", () =>{
